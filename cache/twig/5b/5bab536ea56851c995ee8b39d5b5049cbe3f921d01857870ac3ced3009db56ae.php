16fad078aa6fbfe3f8d7fcb146a2d57d626f069d
Fernando Val <aaromnido@gmail.com>
2017-03-01 23:46:26 +0100

    Content

--- cache/twig/5b/5bab536ea56851c995ee8b39d5b5049cbe3f921d01857870ac3ced3009db56ae.php
+++ cache/twig/5b/5bab536ea56851c995ee8b39d5b5049cbe3f921d01857870ac3ced3009db56ae.php
@@ -28,9 +28,16 @@ class __TwigTemplate_49e40a90e61cbe337660734e13f46f464780479418b1c436172031536ee
     public function block_content($context, array $blocks = array())
     {
         // line 4
-        echo "\t";
+        echo "\t<div id=\"content\">
+\t\t<h1>";
+        // line 5
+        echo $this->getAttribute(($context["page"] ?? null), "title", array());
+        echo "</h1>
+\t\t";
+        // line 6
         echo $this->getAttribute(($context["page"] ?? null), "content", array());
         echo "
+\t</div>
 ";
     }
 
@@ -46,7 +53,7 @@ class __TwigTemplate_49e40a90e61cbe337660734e13f46f464780479418b1c436172031536ee
 
     public function getDebugInfo()
     {
-        return array (  31 => 4,  28 => 3,  11 => 1,);
+        return array (  38 => 6,  34 => 5,  31 => 4,  28 => 3,  11 => 1,);
     }
 
     /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
@@ -62,7 +69,10 @@ class __TwigTemplate_49e40a90e61cbe337660734e13f46f464780479418b1c436172031536ee
         return new Twig_Source("{% extends 'partials/base.html.twig' %}
 
 {% block content %}
-\t{{ page.content }}
+\t<div id=\"content\">
+\t\t<h1>{{ page.title }}</h1>
+\t\t{{ page.content }}
+\t</div>
 {% endblock %}
 ", "default.html.twig", "/Applications/MAMP/htdocs/electricvehicles/user/themes/ev-matter/templates/default.html.twig");
     }
