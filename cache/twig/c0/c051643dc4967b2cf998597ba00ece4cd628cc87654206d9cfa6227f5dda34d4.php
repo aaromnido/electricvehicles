4be9c28b73c62fd1da987aa03533b36881f94291
Fernando Val <aaromnido@gmail.com>
2017-03-01 19:01:58 +0100

    ev-matter child theme

---
+++ cache/twig/c0/c051643dc4967b2cf998597ba00ece4cd628cc87654206d9cfa6227f5dda34d4.php
@@ -0,0 +1,70 @@
+<?php
+
+/* theme.php.twig */
+class __TwigTemplate_bd130fcb063d86f11e5109aedbfd7da7ec00375487586c467ca4dcb2011efeef extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<?php
+namespace Grav\\Theme;
+
+use Grav\\Common\\Theme;
+
+class ";
+        // line 6
+        echo $this->env->getExtension('Grav\Common\Twig\TwigExtension')->inflectorFilter("camel", $this->getAttribute((isset($context["component"]) ? $context["component"] : null), "name", array()));
+        echo " extends Theme
+{
+    // Access plugin events in this class
+}
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "theme.php.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  26 => 6,  19 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("<?php
+namespace Grav\\Theme;
+
+use Grav\\Common\\Theme;
+
+class {{ component.name|camelize }} extends Theme
+{
+    // Access plugin events in this class
+}
+", "theme.php.twig", "/Applications/MAMP/htdocs/electricvehicles/user/plugins/devtools/components/theme/inheritence/theme.php.twig");
+    }
+}
