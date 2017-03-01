bd86ea1607da0a1e3921626cd7a3a54c725a9838
Fernando Val <aaromnido@gmail.com>
2017-03-01 13:06:57 +0100

    Initial commit

---
+++ cache/compiled/files/042afbca52239adbc36fd6c3b4f28462.yaml.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Applications/MAMP/htdocs/grav-electric-vehicles/user/plugins/problems/problems.yaml',
+    'modified' => 1487333390,
+    'data' => [
+        'enabled' => true,
+        'built_in_css' => true
+    ]
+];
---
+++ cache/compiled/files/726490b6a6034e25dcabef48c8d004bd.yaml.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Applications/MAMP/htdocs/grav-electric-vehicles/user/config/security.yaml',
+    'modified' => 1488363515,
+    'data' => [
+        'salt' => '84hqe9WgcGIYlj'
+    ]
+];
