742085d2ba57e5c24ec851c65d2cae5dc65516ac
Fernando Val <aaromnido@gmail.com>
2017-03-03 00:47:40 +0100

    Sergio user improvements

--- cache/compiled/files/833f70def13ba0aa9816e426470ccd85.yaml.php
+++ cache/compiled/files/833f70def13ba0aa9816e426470ccd85.yaml.php
@@ -2,21 +2,26 @@
 return [
     '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
     'filename' => '/Applications/MAMP/htdocs/electricvehicles/user/accounts/sergio.yaml',
-    'modified' => 1488497113,
+    'modified' => 1488498408,
     'data' => [
         'email' => 'sergio@electricvehicles.es',
         'access' => [
             'admin' => [
-                'login' => true,
-                'super' => true
+                'super' => 'false',
+                'login' => 'true',
+                'pages' => 'true',
+                'maintenance' => 'true',
+                'statistics' => 'true',
+                'plugins' => 'false'
             ],
             'site' => [
-                'login' => true
+                'login' => 'true'
             ]
         ],
         'fullname' => 'Sergio García',
         'title' => 'Administrator',
         'state' => 'enabled',
-        'hashed_password' => '$2y$10$l.QrWruD5KhayZQcJ9lAAuVtb8D0frD.yWWDDDaH2kvpHDuCvpsCW'
+        'hashed_password' => '$2y$10$l.QrWruD5KhayZQcJ9lAAuVtb8D0frD.yWWDDDaH2kvpHDuCvpsCW',
+        'language' => 'es'
     ]
 ];
